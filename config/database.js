c69a61ab4fae543e30e1ff13bc61e3f615513531
girishgodage <girishgodage@gmail.com>
2018-07-01 09:50:29 +0530

    Code Change for Prod Deployment

---
+++ config/database.js
@@ -0,0 +1,11 @@
+if(process.env.Node_ENV=== "prodution")
+{
+    module.exports = {
+        mongoURI : 'mongodb://girish:g1r1sh@ds123971.mlab.com:23971/giri_vidjot_prod'
+    }
+} else {
+    module.exports = {
+        mongoURI : 'mongodb://localhost/vidjot-dev'
+    }
+}
+
