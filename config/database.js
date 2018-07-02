096cb91eecd0c18751b56212c31c1b6c3a81b9fe
girishgodage <girishgodage@gmail.com>
2018-07-02 17:55:43 +0530

    make both db connecction to connect m-lab db

--- config/database.js
+++ config/database.js
@@ -5,7 +5,9 @@ if(process.env.Node_ENV === "production")
     }
 } else {
     module.exports = {
-        MONGODB_URI : 'mongodb://localhost/vidjot-dev'
+        // MONGODB_URI : 'mongodb://localhost/vidjot-dev'
+        MONGODB_URI : 'mongodb://girish:g1r1sh@ds123971.mlab.com:23971/giri_vidjot_prod'
+        
     }
 }
 
